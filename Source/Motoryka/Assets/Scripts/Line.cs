b7e2ddbd0074bd04b968158f0edb5ce53a1ffd1b
wnadurski <hysterus@gmail.com>
2015-07-29 22:39:18 +0200

    Created directory tree and new classes responsible for line rendering

---
+++ Source/Motoryka/Assets/Scripts/Line.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Line : MonoBehaviour 
+{
+    private LineRenderer _lineRenderer;
+    private Vector3 _defaultPosition;
+    private List<Vector3> _vertices;
+    private int _lrVerticesCount;
+    private float _size;
+    private float _lrSize;
+
+    public Line()
+    {
+        _vertices = new List<Vector3>();
+        _lrVerticesCount = 0;
+        _size = 1;
+        _lrSize = 1;
+    }
+
+	// Use this for initialization
+	void Start () {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _defaultPosition = Vector3.zero;
+	}
+
+    void Update()
+    {
+        while(_lrVerticesCount < _vertices.Count)
+        {
+            _lineRenderer.SetVertexCount(_lrVerticesCount + 1);
+            _lineRenderer.SetPosition(_lrVerticesCount, _vertices[_lrVerticesCount]);
+
+            _lrVerticesCount++;
+        }
+
+        if (_size != _lrSize)
+        {
+            _lrSize = _size;
+            _lineRenderer.SetWidth(_lrSize, _lrSize);
+        }
+    }
+
+    public void AddVertex(Vector2 pos)
+    {
+        AddVertex(new Vector3(pos.x, pos.y, _defaultPosition.z));
+    }
+
+    public void AddVertex(Vector3 pos)
+    {
+        pos.z = _defaultPosition.z;
+        _vertices.Add(pos);
+    }
+    public void SetSize(float size)
+    {
+        _size = size;
+    }
+
+    public int vertexCount
+    {
+        get
+        {
+            return _vertices.Count;
+        }
+    }
+
+    
+}
---
+++ Source/Motoryka/Assets/Scripts/Test.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+    public bool drawing = false;
+    public Line currentlyDrawing = null;
+    public LineFactory lf;
+    public Camera camera;
+    public Vector3 prevVertex;
+	// Use this for initialization
+	void Start () {
+        lf = new LineFactory();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+	    if(Input.GetMouseButtonDown(0))
+        {
+            drawing = true;
+            if(currentlyDrawing == null)
+            {
+
+                prevVertex = camera.ScreenToWorldPoint(Input.mousePosition);
+                currentlyDrawing = lf.Create(prevVertex);
+                currentlyDrawing.SetSize(0.1f);
+            }
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            drawing = false;
+            currentlyDrawing = null;
+        }
+
+        if(drawing && currentlyDrawing != null)
+        {
+            Vector3 newPos = camera.ScreenToWorldPoint(Input.mousePosition);
+            if(Vector3.Distance(newPos, prevVertex) > 0.2)
+            {
+                currentlyDrawing.AddVertex(newPos);
+                prevVertex = newPos;
+            }
+        }
+	}
+}
