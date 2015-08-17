d2fbce25c730dcb2120725f684f97d99135db74c
wnadurski <w.nadurski@gmail.com>
2015-08-17 17:36:49 +0200

    LineDrawer finish
    finished classes to draw lines

---
+++ Source/Motoryka/Assets/Scripts/LineManagement/ILine.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface ILine {
+    void Init(Transform canvas);
+    int VertexCount { get; }
+    Color Color { get; }
+    void SetColor(Color color);
+    void SetSize(float size);
+    void AddVertex(Vector2 pos);
+    void AddVertex(Vector3 pos);
+}
---
+++ Source/Motoryka/Assets/Scripts/TestingScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestingScript : MonoBehaviour {
+    public LineDrawer drawer;
+    public Camera cam;
+    public Color color;
+
+	// Use this for initialization
+	void Start () {
+        drawer.SetSize(0.2f);
+        drawer.SetColor(color);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //Graphics.DrawProcedural(MeshTopology.Quads())
+	    if(Input.GetMouseButtonDown(0))
+        {
+            drawer.StartDrawing();
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            drawer.StopDrawing();
+        }
+
+        if(drawer.IsDrawing)
+        {
+            drawer.Draw(cam.ScreenToWorldPoint(Input.mousePosition));
+        }
+	}
+}
