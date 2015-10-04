86b321a1374d0ca84c60e83e93d012cb5bdeaa5c
wnadurski <w.nadurski@gmail.com>
2015-10-04 22:55:35 +0200

    Some Fireworks and GameManager
    Created some BaseLevelManagers classes, GameManager class, Fader class
    which is a firework for FadeOut FadeIn scenes.

---
+++ Source/Motoryka/Assets/Scripts/Fireworks/Fader.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Fader : MonoBehaviour {
+    public Image fadingTexture;
+    public float fadingSpeed = 2f;
+    public bool isFading = false;
+
+    float _alpha = 0f;
+
+    void Start()
+    {
+        isFading = false;
+        _alpha = 0f;
+    }
+
+	public void LoadSceneFading(string scene)
+    {
+        if( !fadingTexture )
+        {
+            Debug.LogError("No texture to fade.");
+            return;
+        }
+
+        Debug.Log("Fading to scene " + scene);
+        StartCoroutine("LoadFading", scene);
+    }
+
+    private IEnumerator LoadFading(string scene)
+    {
+        isFading = true;
+
+        float delay = 0.1f;
+        
+        _alpha = 0f;
+        fadingTexture.color = new Color(fadingTexture.color.r, fadingTexture.color.g, fadingTexture.color.b, _alpha);
+
+        while(_alpha < 1f)
+        {
+            yield return new WaitForEndOfFrame();
+
+            _alpha += Time.deltaTime * fadingSpeed;
+            fadingTexture.color = new Color(fadingTexture.color.r, fadingTexture.color.g, fadingTexture.color.b, _alpha);
+        }
+
+        fadingTexture.color = new Color(fadingTexture.color.r, fadingTexture.color.g, fadingTexture.color.b, 1f);
+
+        yield return new WaitForSeconds(delay/2);
+
+        Application.LoadLevel(scene);
+
+        yield return new WaitForSeconds(delay / 2);
+
+        while (_alpha > 0)
+        {
+            _alpha -= Time.deltaTime * fadingSpeed;
+            fadingTexture.color = new Color(fadingTexture.color.r, fadingTexture.color.g, fadingTexture.color.b, _alpha);
+
+            yield return new WaitForEndOfFrame();
+        }
+        fadingTexture.color = new Color(fadingTexture.color.r, fadingTexture.color.g, fadingTexture.color.b, 0f);
+
+
+        isFading = false;
+    }
+}
---
+++ Source/Motoryka/Assets/Scripts/Managers/BaseLvlManager.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+public enum LevelPhase
+{
+    Paused,
+    Prestarted,
+    Started,
+    Running,
+    Prefinished,
+    Finished
+}
+
+public class BaseLvlManager<T> : Singleton<T> where T : MonoBehaviour
+{
+    LevelPhase _phase = LevelPhase.Prestarted;
+
+    public LevelPhase CurrentPhase {
+        get
+        {
+            return _phase;
+        }
+        set
+        {
+            if (_phase == LevelPhase.Running && value == LevelPhase.Paused)
+            {
+                _phase = value;
+            }
+            else if (_phase == LevelPhase.Paused && value == LevelPhase.Running)
+            {
+                _phase = value;
+            }
+            else if (_phase == LevelPhase.Prestarted && value ==LevelPhase.Started)
+            {
+                PreStart();
+                _phase = value;
+            }
+            else if (_phase == LevelPhase.Prestarted && value == LevelPhase.Running)
+            {
+                PreStart();
+                PostStart();
+                _phase = value;
+            }
+            else if (_phase == LevelPhase.Started && value == LevelPhase.Running)
+            {
+                PostStart();
+                _phase = value;
+            }
+            else if (_phase == LevelPhase.Running && value == LevelPhase.Prefinished)
+            {
+                PreFinish();
+                _phase = value;
+            }
+            else if (_phase == LevelPhase.Running && value == LevelPhase.Finished)
+            {
+                PreFinish();
+                PostFinish();
+                _phase = value;
+            }
+            else if (_phase == LevelPhase.Prefinished && value == LevelPhase.Finished)
+            {
+                PostFinish();
+                _phase = value;
+            }
+        }
+    }
+
+    protected virtual void PreStart()
+    {
+
+    }
+
+    protected virtual void PostStart()
+    {
+
+    }
+
+    protected virtual void PreFinish()
+    {
+
+    }
+
+    protected virtual void PostFinish() 
+    {
+
+    }
+
+}
+
