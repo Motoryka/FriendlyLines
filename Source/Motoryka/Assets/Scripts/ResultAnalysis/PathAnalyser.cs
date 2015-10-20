2912d219aaa262dfe470f9d0afa9f7fdc380620c
magdafox <magda.foks@gmail.com>
2015-10-20 18:56:03 +0200

    Fixing PathAnalyser - first and last point

--- Source/Motoryka/Assets/Scripts/ResultAnalysis/PathAnalyser.cs
+++ Source/Motoryka/Assets/Scripts/ResultAnalysis/PathAnalyser.cs
@@ -15,24 +15,43 @@ public class PathAnalyser : IAnalyser {
 
 		bool isChecked = false;
 
-		foreach (Vector3 checkpoint in generatedLine.GetVerticles()) {
+		if (AreFinalPointsCorrect(generatedLine, userLine)) {
+			foreach (Vector3 checkpoint in generatedLine.GetVerticles()) {
 
-			foreach(Vector3 point in userLine.GetVerticles()) {
+				foreach(Vector3 point in userLine.GetVerticles()) {
 
-				float _distance = Vector3.Distance(point, checkpoint);
+					float _distance = Vector3.Distance(point, checkpoint);
 
-				if (_distance < _acceptedError) {
-					isChecked = true;
-					break;
+					if (_distance < _acceptedError) {
+						isChecked = true;
+						break;
+					}
 				}
-			}
 
-			if (!isChecked) {
-				return false;
+				if (!isChecked) {
+					return false;
+				}
+				isChecked = false;
 			}
-			isChecked = false;
+			return true;
 		}
 
-		return true;
+		return false;
+	}
+
+	private bool AreFinalPointsCorrect (ILine generatedLine, ILine userLine) {
+		Vector3[] listG = generatedLine.GetVerticles ().ToArray ();
+		Vector3[] listU = userLine.GetVerticles ().ToArray ();
+		
+		if (listG.Length == 0 || listU.Length == 0)
+			return false;
+		
+		if (Vector3.Distance(listG[0], listU[0]) < _acceptedError &&
+		    Vector3.Distance(listG[listG.Length-1], listU[listU.Length-1]) < _acceptedError) {
+			
+			return true;
+		}
+		
+		return false;
 	}
 }
