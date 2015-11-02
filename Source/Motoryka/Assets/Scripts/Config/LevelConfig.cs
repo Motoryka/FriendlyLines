89c01594d2d77ebbb7f832697c63e1cfdf4dfc3d
nojas10 <nojasek@gmail.com>
2015-11-02 18:54:37 +0100

    New Config class
    Fixes some bugs and adds LevelConfig class to Config.

---
+++ Source/Motoryka/Assets/Scripts/Config/LevelConfig.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelConfig 
+{
+	public int levelNumber { get; set; }
+	public Shape shape { get; set; }
+	public float shapeStroke { get; set; }
+	public float brushStroke { get; set; }
+	public Color shapeColor { get; set; }
+	public Color brushColor { get; set; }
+	public int difficulty { get; set; }
+}
