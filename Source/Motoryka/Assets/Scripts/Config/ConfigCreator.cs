d8f1a2bcd68cc8f96812f147020548b2540881aa
wnadurski <w.nadurski@gmail.com>
2015-11-10 00:39:40 +0100

    Config Creation scene

--- Source/Motoryka/Assets/Scripts/Config/ConfigCreator.cs
+++ Source/Motoryka/Assets/Scripts/Config/ConfigCreator.cs
@@ -1,7 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ConfigCreator : MonoBehaviour {
+    public GameObject canvas;
+    public GameObject uiLevelManagerPrefab;
+
+    Config config;
+    List<UILevelManager> _levelManagers;
+    int activeLevelManager;
+
+    Vector3 previousPoint = new Vector3(-Screen.width, 0f, 0f);
+    Vector3 activePoint = Vector3.zero;
+    Vector3 nextPoint = new Vector3(Screen.width, 0f, 0f);
 
 	public T GetUIElement<T>(string name)
 	{
@@ -9,13 +20,34 @@ public class ConfigCreator : MonoBehaviour {
 		var element = go.GetComponent<T>();
 		return (T) element;
 	}
-    Config config;
 
 	// Use this for initialization
 	void Start () {
-		var shapeText = GetUIElement<ComboBox>("ShapeText");
-		int shape = shapeText.SelectedOption;
         config = ConfigFactory.CreateEasyLevel();
+
+        _levelManagers = new List<UILevelManager>();
+        int i = 0;
+
+
+        foreach(LevelConfig l in config.Levels)
+        {
+            GameObject o = Instantiate<GameObject>(uiLevelManagerPrefab);
+            o.transform.SetParent(canvas.transform);
+            o.transform.localPosition = nextPoint * i;
+            /*
+            if (i == 0)
+                o.transform.localPosition = activePoint;
+            else
+                o.transform.localPosition = nextPoint;*/
+            UILevelManager mng = o.GetComponent<UILevelManager>();
+
+            mng.Init(this, l);
+
+            _levelManagers.Add(mng);
+            i++;
+        }
+
+        activeLevelManager = 0;
 	}
 	
 	// Update is called once per frame
@@ -31,36 +63,40 @@ public class ConfigCreator : MonoBehaviour {
 
     public void SetActiveLevelNext()
     {
-
+        if (activeLevelManager < _levelManagers.Count -1)
+        {
+            foreach (var mng in _levelManagers)
+            {
+                mng.MoveToPoint(mng.transform.localPosition + previousPoint);
+            }
+            activeLevelManager++;
+        /*
+            _levelManagers[activeLevelManager].MoveToPoint(previousPoint);
+            activeLevelManager++;
+            _levelManagers[activeLevelManager].MoveToPoint(activePoint);*/
+        }
     }
 
     public void SetActiveLevelPrevious()
     {
+        if (activeLevelManager > 0)
+        {
 
+            foreach (var mng in _levelManagers)
+            {
+                mng.MoveToPoint(mng.transform.localPosition + nextPoint);
+            }
+            activeLevelManager--;
+            /*
+            _levelManagers[activeLevelManager].MoveToPoint(nextPoint);
+            activeLevelManager--;
+            _levelManagers[activeLevelManager].MoveToPoint(activePoint);*/
+        }
     }
 
-    public void SetShape()
+    public void SaveConfig()
     {
-
+        GameManager.Instance.fader.LoadSceneFading("title");
     }
-
-    public void SetShapeStroke()
-    {
-
-    }
-
-    public void SetBrushStroke()
-    {
-
-    }
-
-    public void SetShapeColor()
-    {
-
-    }
-
-    public void SetBrushColor()
-    {
-
-    }
+    
 }
