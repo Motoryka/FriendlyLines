a51aa7247f529ae9ff9e18e8f5f04a3d8e79de62
nojas10 <nojasek@gmail.com>
2015-11-11 12:38:34 +0100

    Some config factory changes for presentation.

--- Source/Motoryka/Assets/Scripts/Config/ConfigFactory.cs
+++ Source/Motoryka/Assets/Scripts/Config/ConfigFactory.cs
@@ -17,7 +17,7 @@ public class ConfigFactory
 			NrOfLevels = 3,
 			Levels = new List<LevelConfig> {
 				new LevelConfig { levelNumber = 1, shape = Shape.VerticalLine, shapeStroke = LineStroke.VeryThick, brushStroke = LineStroke.VeryThick, shapeColor = PastelColorFactory.Blue.Color, brushColor = PastelColorFactory.LightBlue.Color, difficulty = 2 },
-				new LevelConfig { levelNumber = 2, shape = Shape.VerticalLine, shapeStroke = LineStroke.VeryThick, brushStroke = LineStroke.VeryThick, shapeColor = PastelColorFactory.Yellow.Color, brushColor = PastelColorFactory.LightRed.Color, difficulty = 2 },
+				new LevelConfig { levelNumber = 2, shape = Shape.HorizontalLine, shapeStroke = LineStroke.VeryThick, brushStroke = LineStroke.VeryThick, shapeColor = PastelColorFactory.Yellow.Color, brushColor = PastelColorFactory.LightRed.Color, difficulty = 2 },
 				new LevelConfig { levelNumber = 3, shape = Shape.DiagonalLine, shapeStroke = LineStroke.Thick, brushStroke = LineStroke.Thick, shapeColor = PastelColorFactory.Green.Color, brushColor = PastelColorFactory.Gray.Color, difficulty = 2 }
 			}
 		};
@@ -31,11 +31,17 @@ public class ConfigFactory
 			Name = "Medium Level",
 			CreationDate = DateTime.Now,
 			DrawStartPoint = true,
-			NrOfLevels = 3,
+			NrOfLevels = 9,
 			Levels = new List<LevelConfig> {
-				new LevelConfig { levelNumber = 1, shape = Shape.CurvedLine, shapeStroke = LineStroke.Medium, brushStroke = LineStroke.Medium, shapeColor = Color.magenta, brushColor = Color.black, difficulty = 2 },
-				new LevelConfig { levelNumber = 2, shape = Shape.Circle, shapeStroke = LineStroke.Medium, brushStroke = LineStroke.Medium, shapeColor = PastelColorFactory.DarkBlue.Color, brushColor = PastelColorFactory.LightPink.Color, difficulty = 2 },
-				new LevelConfig { levelNumber = 3, shape = Shape.Ellipse, shapeStroke = LineStroke.Thin, brushStroke = LineStroke.Thin, shapeColor = PastelColorFactory.Purple.Color, brushColor = PastelColorFactory.LightGreen.Color, difficulty = 2 }
+				new LevelConfig { levelNumber = 1, shape = Shape.VerticalLine, shapeStroke = LineStroke.VeryThick, brushStroke = LineStroke.VeryThick, shapeColor = PastelColorFactory.LightYellow.Color, brushColor = PastelColorFactory.LightBlue.Color, difficulty = 2 },
+				new LevelConfig { levelNumber = 2, shape = Shape.HorizontalLine, shapeStroke = LineStroke.Thick, brushStroke = LineStroke.Thick, shapeColor = PastelColorFactory.DarkBlue.Color, brushColor = PastelColorFactory.LightPink.Color, difficulty = 2 },
+				new LevelConfig { levelNumber = 3, shape = Shape.DiagonalLine, shapeStroke = LineStroke.Medium, brushStroke = LineStroke.Medium, shapeColor = PastelColorFactory.Purple.Color, brushColor = PastelColorFactory.LightGreen.Color, difficulty = 2 },
+				new LevelConfig { levelNumber = 4, shape = Shape.CurvedLine, shapeStroke = LineStroke.Thin, brushStroke = LineStroke.Thin, shapeColor = PastelColorFactory.Green.Color, brushColor = PastelColorFactory.DarkGray.Color, difficulty = 2 },
+				new LevelConfig { levelNumber = 5, shape = Shape.Circle, shapeStroke = LineStroke.VeryThin, brushStroke = LineStroke.VeryThin, shapeColor = PastelColorFactory.Blue.Color, brushColor = PastelColorFactory.LightRed.Color, difficulty = 2 },
+				new LevelConfig { levelNumber = 6, shape = Shape.Ellipse, shapeStroke = LineStroke.Thin, brushStroke = LineStroke.Thin, shapeColor = PastelColorFactory.Mint.Color, brushColor = PastelColorFactory.Black.Color, difficulty = 2 },
+				new LevelConfig { levelNumber = 7, shape = Shape.Triangle, shapeStroke = LineStroke.Thin, brushStroke = LineStroke.Thin, shapeColor = PastelColorFactory.Orange.Color, brushColor = PastelColorFactory.Yellow.Color, difficulty = 2 },
+				new LevelConfig { levelNumber = 8, shape = Shape.Square, shapeStroke = LineStroke.Thin, brushStroke = LineStroke.Thin, shapeColor = PastelColorFactory.LightBlue.Color, brushColor = PastelColorFactory.LightRed.Color, difficulty = 2 },
+				new LevelConfig { levelNumber = 9, shape = Shape.Rectangle, shapeStroke = LineStroke.Thin, brushStroke = LineStroke.Thin, shapeColor = PastelColorFactory.Purple.Color, brushColor = PastelColorFactory.LightGreen.Color, difficulty = 2 }
 			}
 		};
 		return config;
