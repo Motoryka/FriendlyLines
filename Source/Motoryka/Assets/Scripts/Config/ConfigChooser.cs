598ca8136a00d9728c2a830e008267d28ad17d99
jezyna <jezyna.domanska@gmail.com>
2015-11-28 20:02:32 +0100

    animacja przycisków z poziomami trudności

--- Source/Motoryka/Assets/Scripts/Config/ConfigChooser.cs
+++ Source/Motoryka/Assets/Scripts/Config/ConfigChooser.cs
@@ -82,7 +82,10 @@ public class ConfigChooser : MonoBehaviour {
 			var btn = o.transform.GetComponent<Button>();
 			if(btn != null){
 				if(btn.name == buttonName){
-					btn.image.color = PastelColorFactory.Mint.Color;
+					if(btn.animator != null)
+						btn.animator.SetBool("isChosen", true);
+					else 
+						btn.image.color = PastelColorFactory.Mint.Color;
 				}
 			}
 		}
@@ -150,7 +153,10 @@ public class ConfigChooser : MonoBehaviour {
 	public void ChooseSelectedButton(Button button)
 	{
 		RemoveSelectionOnButton();
-		button.image.color = PastelColorFactory.Mint.Color;
+		if(button.animator != null)
+			button.animator.SetBool ("isChosen", true);
+		else 
+			button.image.color = PastelColorFactory.Mint.Color;
 	}
 
 	private void RemoveSelectionOnButton()
@@ -162,6 +168,9 @@ public class ConfigChooser : MonoBehaviour {
 				if(btn.image.color == PastelColorFactory.Mint.Color){
 					btn.image.color = Color.white;
 				}
+				if (btn.animator != null)
+					btn.animator.SetBool ("isChosen", false);
+
 			}
 		}
 	}
